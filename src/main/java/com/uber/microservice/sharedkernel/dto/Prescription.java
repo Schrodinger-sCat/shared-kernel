298d43c5b3c08fca8c271ed150475263e32b0393
Md. Giash Uddin <giash.inument@bkash.com>
2025-12-27 11:39:50 +0600

    Added NoArgsConstructor dtos

--- src/main/java/com/uber/microservice/sharedkernel/dto/Prescription.java
+++ src/main/java/com/uber/microservice/sharedkernel/dto/Prescription.java
@@ -2,11 +2,13 @@ package com.uber.microservice.sharedkernel.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
